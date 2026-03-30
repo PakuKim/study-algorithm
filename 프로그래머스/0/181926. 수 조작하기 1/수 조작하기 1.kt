65a635c32ba5f467687d2114104110b5a490622f
Jeongwoo Kim <saclen48@gmail.com>
2026-03-30 23:07:15 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.13 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int, control: String): Int {
+        var answer: Int = n
+        for (w in control) {
+            when(w) {
+                'w' -> answer += 1
+                's' -> answer -= 1
+                'd' -> answer += 10
+                'a' -> answer -= 10
+            }
+        }
+        return answer
+    }
+}
