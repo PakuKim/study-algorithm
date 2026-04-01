cf9464c62961fd8a189f4cfe019d7901183897de
Jeongwoo Kim <saclen48@gmail.com>
2026-04-01 17:14:35 +0900

    [level 2] Title: 배달, Time: 7.55 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.kt
+++ 프로그래머스/lv2/12978. 배달/배달.kt
@@ -3,36 +3,30 @@ import java.util.*
 class Solution {
     fun solution(N: Int, road: Array<IntArray>, k: Int): Int {
         val graph = mutableMapOf<Int, MutableList<Pair<Int,Int>>>()
-
         for ((a,b,c) in road) {
             graph.getOrPut(a) { mutableListOf() }.add(b to c)
             graph.getOrPut(b) { mutableListOf() }.add(a to c)
         }
         
-        fun dijkstra(n: Int): IntArray {
-            val costs = IntArray(N + 1) { Int.MAX_VALUE }
-            val queue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
-            
-            costs[1] = 0
-            queue.add(1 to 0)
-            
-            while (queue.isNotEmpty()) {
-                val (node, cost) = queue.poll()!!
-                if (cost > costs[node]) continue
-                
-                for ((next, nextCost) in graph[node]!!) {
-                    val newCost = cost + nextCost
-                    
-                    if (newCost < costs[next]) {
-                        costs[next] = newCost
-                        queue.add(next to newCost)
-                    }
+        val costs = IntArray(N + 1) { Int.MAX_VALUE }
+        val queue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+        costs[1] = 0
+        queue.add(1 to 0)
+        
+        while (queue.isNotEmpty()) {
+            val (node, cost) = queue.poll()!!
+            if (cost > costs[node]) continue
+
+            for ((next, nextCost) in graph[node]!!) {
+                val newCost = cost + nextCost
+
+                if (newCost < costs[next]) {
+                    costs[next] = newCost
+                    queue.add(next to newCost)
                 }
             }
-            
-            return costs
         }
 
-        return dijkstra(N).count { it <= k }
+        return costs.count { it <= k }
     }
 }
