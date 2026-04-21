ccaf62cc37d2547380caf21f040f1a1ccbdcc45d
Jeongwoo Kim <saclen48@gmail.com>
2026-04-21 18:02:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Set<String> used = new HashSet<>();
+        char last = words[0].charAt(0);
+        
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].charAt(0) != last || used.contains(words[i])) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }
+            used.add(words[i]);
+            last = words[i].charAt(words[i].length() - 1);
+        }
+
+        return answer;
+    }
+}
