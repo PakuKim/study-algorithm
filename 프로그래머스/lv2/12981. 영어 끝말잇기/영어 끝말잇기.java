244148555883e824bb39853da00e586952e2f10d
Jeongwoo Kim <saclen48@gmail.com>
2026-04-21 18:03:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -7,12 +7,11 @@ class Solution {
         char last = words[0].charAt(0);
         
         for (int i = 0; i < words.length; i++) {
-            if (words[i].charAt(0) != last || used.contains(words[i])) {
+            if (words[i].charAt(0) != last || !used.add(words[i])) {
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
             }
-            used.add(words[i]);
             last = words[i].charAt(words[i].length() - 1);
         }
 
