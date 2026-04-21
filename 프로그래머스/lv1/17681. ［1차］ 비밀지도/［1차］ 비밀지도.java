693e021b0f9c03be9f9c7406bc53d9ec63109a86
Jeongwoo Kim <saclen48@gmail.com>
2026-04-21 17:26:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int combined = arr1[i] | arr2[i];
+            for (int j = n - 1; j >= 0; j--) {
+                sb.append(((combined >> j) & 1) == 1 ? "#" : " ");
+            }
+            answer[i] = sb.toString();
+        }
+
+        return answer;
+    }
+}
